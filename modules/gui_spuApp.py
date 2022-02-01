60eaee310746eef5f08f8d476b4cf0138083a9ec
Oleksandr Siora <alexandrsiora@gmail.com>
2022-02-01 14:51:45 +0200

    - Add the basic template for app

---
+++ modules/gui_spuApp.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class BoxLayoutH(BoxLayout):
+    pass
+
+class BoxLayoutV(BoxLayout):
+    pass
+
+class gui_spuApp(App):
+    pass
+
+gui_spuApp().run()
